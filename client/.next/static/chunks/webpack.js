aded09d4c05c272c1529584753faf9fe12f48d85
JOON HYUK LEE <60730765+anshqhsh@users.noreply.github.com>
2022-03-04 20:47:51 +0900

    express Server - REST API 구성
    messages, users 에 관한 express server 구성

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1afaa715d939b5fb"; }
+/******/ 		__webpack_require__.h = function() { return "8e55545bdd444ba0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ server/src/dbController.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+// import fs =require('fs') pakage.json   "type": "module", 을 이용해 ES6 파일시스템 사용 가능
+import { resolve } from 'path';
+
+// 서버의 라우트에서 read/write를 결정해서 해주면 된다.
+const basePath = resolve(); // 현재 경로가 basePath
+
+const filenames = {
+  messages: resolve(basePath, 'src/db/messages.json'),
+  users: resolve(basePath, 'src/db/users.json'),
+};
+
+export const readDB = target => {
+  console.log(filenames[target]);
+  try {
+    return JSON.parse(fs.readFileSync(filenames[target], 'utf-8'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const writeDB = (target, data) => {
+  try {
+    return fs.writeFileSync(filenames[target], JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
